d5c1b0d1a7f6fc7e75acee25e7291174aaa37879
Benedikt <144247685+b-at-neu@users.noreply.github.com>
2025-12-03 13:56:15 -0500

    remove contact from header

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,6 @@ const navigation = [
   { name: "Home", href: "#home" },
   { name: "About", href: "#about" },
   { name: "Projects", href: "#projects" },
-  { name: "Contact", href: "#contact" },
 ];
 
 export default function Header() {
